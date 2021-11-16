1458b03546b99fe98865b8114cf843e4581076f2
Bohdan Kovtun <bohdankovt@tahometer.com>
2021-11-16 18:16:16 +0200

    some change

--- main.js
+++ main.js
@@ -11,7 +11,6 @@ function createTask(task) {
 
     let taskHeader = createTaskHeader(task);
     let taskBody = createTaskBody(task);
-    taskBody.append(createCollapseEditTaskBody(task))
 
     tasksElement.append(taskHeader, taskBody);
     return tasksElement;
@@ -24,7 +23,7 @@ function createTaskHeader(task) {
                         <h5 class="card-title">${task.title} <i class="bi bi-check-all"></i></h5>
                         <div class="card-actions">
                             <span class="action-content"></span>
-                            <i class="bi bi-pencil-square" data-toggle="collapse" data-target="#task${task.id}" aria-expanded="false" aria-controls="task${task.id}"></i>
+                            <i class="bi bi-pencil-square edit"></i>
                             <i class="bi bi-trash remove-icon"></i>
                         </div>`;
     
@@ -34,6 +33,9 @@ function createTaskHeader(task) {
     let removeIcon = header.querySelector('.remove-icon');
     removeIcon.addEventListener('click', deleteElem);
     
+    let editIcon = header.querySelector('.edit');
+    editIcon.addEventListener('click', function(){ createModalForTask(task)})
+
     if(task.is_done) {
         header.classList.add('bg-success');
         let title = header.querySelector('h5');
@@ -41,10 +43,8 @@ function createTaskHeader(task) {
 
         let checkIcon = title.querySelector('i');
         checkIcon.style.display = 'inline';
-
-        
     }
-    else if(task.exp_date.toDate() < moment().toDate()) {
+    else if(!task.exp_date.isSameOrAfter(moment(), 'date')) {
         header.classList.add('bg-danger');
 
         let duration = moment.duration(moment().diff(moment(task.exp_date)));
@@ -62,13 +62,6 @@ function createTaskBody(task) {
     body.innerHTML = `${task.desc}<hr/>`;
     return body;
 }
-function createCollapseEditTaskBody(task) {
-    let collapse = document.createElement('div')
-    collapse.classList.add('collapse', 'mt-3')
-    collapse.id =  `task${task.id}`;
-    collapse.innerHTML += 'fvfvfvfv'
-    return collapse;
-}
 function taskValidDateInterval(a, b) {
     return `${moment(a).format('MMMM D')} - ${moment(b).format('MMMM D')}`
 }
@@ -79,6 +72,47 @@ function showTasks() {
     if(this.checked) tasksContainer.classList.add('show-done')
     else tasksContainer.classList.remove('show-done')
     
+}
+function createModalForTask(task) {
+
+    taskEditModal.querySelector('.new-title').value = task.title;
+    taskEditModal.querySelector('.new-desc').value = task.desc;
+    taskEditModal.querySelector('.new-exp-date').value = task.exp_date.format('YYYY-MM-DD');
+    
+
+    // taskEditModal.querySelector('.modal-footer').innerHTML += `<input class="new-is-done" type="checkbox" data-toggle="toggle" data-width="100" data-size="xs" data-on="Ready" data-off="Not Ready" data-onstyle="success" data-offstyle="danger">`
+
+
+
+    let t = $('.new-is-done')
+    if(task.is_done) {
+        t.bootstrapToggle('on')
+    }else{
+        t.bootstrapToggle('off')
+    }
+
+    $('#taskEditModal').modal('show')
+
+    // let saveButton = taskEditModal.querySelector('.save__button__edit__task');
+    // saveButton.addEventListener('click', () => saveChanges(task))
+
+
+}
+
+function saveChanges(task) {
+
+    
+    console.log(this)
+    
+    task.title = taskElement.querySelector('.new-title').value;
+    task.desc = taskElement.querySelector('.new-desc').value;
+    // task.exp_date = moment(taskElement.querySelector('.new-exp-date').value)
+    // task.is_done = taskElement.querySelector('.new-is-done').checked ? true : false;
+    
+
+    tasksContainer.replaceChild(createTask(task), repElement)
+
+    // let element = taskElement.querySelector(`#task${taskElement.id}`)
 }
 
 class Task {
@@ -94,13 +128,12 @@ class Task {
 
 let tasks = new Array();
 
-tasks.push(new Task(tasks.length + 1, 'Learn Js', 'Simple learn js', false, moment("2021-11-13")))
+tasks.push(new Task(tasks.length + 1, 'Learn Js', 'Simple learn js', false, moment("2021-11-15")))
 tasks.push(new Task(tasks.length + 1, 'Learn Html', 'Simple learn html', true, moment("2021-12-31")))
 tasks.push(new Task(tasks.length + 1, 'Learn Css', 'Simple learn Css', false, moment("2021-11-20")))
-tasks.push(new Task(tasks.length + 1, 'Some tasks', 'Some descs', false, moment("2021-12-31")))
-tasks.push(new Task(tasks.length + 1, 'Some tasks', 'Some descs', true, moment("2021-12-5")))
-tasks.push(new Task(tasks.length + 1, 'Some tasks', 'Some descs', true, moment("2021-11-30")))
-
+tasks.push(new Task(tasks.length + 1, 'Learn Java', 'Java', false, moment("2021-12-31")))
+tasks.push(new Task(tasks.length + 1, 'Some ASP.NET', 'ASP.NET', true, moment("2021-12-5")))
+tasks.push(new Task(tasks.length + 1, 'Some Learn', 'Learn', true, moment("2021-11-30")))
 
 tasks.sort((a,b) => {
     if (a.is_done < b.is_done) {
@@ -113,17 +146,17 @@ tasks.sort((a,b) => {
 })
 
 let tasksContainer = document.querySelector('.task__section__content__tasks');
+
 //add checkbox listener
 let checkboxAllTask = document.querySelector('.checkbox__All__Task') 
 checkboxAllTask.addEventListener('change', showTasks)
 
-
-let tasksElement = document.getElementsByClassName('task__section__content__tasks')[0];
+let taskEditModal = document.querySelector('#taskEditModal');
 
 tasks.forEach(task => {
 
     let taskElem = createTask(task);
-    tasksElement.append(taskElem);
+    tasksContainer.append(taskElem);
 
 });
 
